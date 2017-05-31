5d83389353b57f67468decd6bd5ef71256be89f4
SanCampos <thedrunkengungan@gmail.com>
2017-05-31 21:21:33 +0800

    feat: add db initialization with Main class

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import db.Database;
+
+/**
+ * Created by thedr on 5/31/2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Database.init();
+    }
+}
---
+++ src/db/Database.java
@@ -0,0 +1,26 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Created by thedr on 5/31/2017.
+ */
+public class Database {
+
+    private static Connection con;
+
+        public static void init() {
+            String url = "jdbc:mysql://localhost:3306/test?verifyServerCertificate=false&useSSL=true";
+            String user = "root";
+            String pass = "root";
+
+            try {
+                con = DriverManager.getConnection(url, user, pass);
+            } catch (SQLException exception) {
+                System.out.println("CONNECTION FAILED: " + exception);
+            }
+
+    }
+}
