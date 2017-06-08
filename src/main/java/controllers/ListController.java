03d095254193741aefdce0c44f4fee2faa8a78df
SanCampos <thedrunkengungan@gmail.com>
2017-06-08 17:13:29 +0800

    attempt one at cancelling table re-ordering CLEANLY (IT AINT WORKING)

--- src/main/java/controllers/ListController.java
+++ src/main/java/controllers/ListController.java
@@ -1,19 +1,21 @@
 package main.java.controllers;
 
 import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import main.java.Main;
 import main.java.db.Database;
+import main.java.models.Employee;
 
 import java.sql.SQLException;
-import java.util.Map;
 
 /**
  * Created by thedr on 6/6/2017.
@@ -22,31 +24,40 @@ public class ListController {
 
     @FXML private ImageView profImg;
     @FXML private Button logout;
+
     @FXML private TableView table;
+    @FXML private TableColumn col_fname;
+    @FXML private TableColumn col_lname;
+    @FXML private TableColumn col_salary;
+    @FXML private TableColumn col_age;
+
 
     @FXML
     public void initialize() {
-        initAvatar();
-        disableReorder();
-
-        Database db = new Database();
-
         try {
-            db.init();
-
-            for (Map<String, String> row : db.getPersonList()) {
-
-            }
-
-        } catch (SQLException e) {
-            System.out.println("Couldn't  connect");
+            loadTableData();
+        } catch (SQLException e){
+            System.out.println("loading  failed");
         }
 
+        initAvatar();
+        disableReorder();
+    }
+
+    private void loadTableData() throws SQLException {
+        Database db = new Database();
+        db.init();
+
+        col_fname.setCellValueFactory(new PropertyValueFactory<Employee, String>("fName"));
+        col_lname.setCellValueFactory(new PropertyValueFactory<Employee, String>("lName"));
+        col_salary.setCellValueFactory(new PropertyValueFactory<Employee, String>("salary"));
+        col_age.setCellValueFactory(new PropertyValueFactory<Employee, String>("age"));
+        table.setItems(db.getEmployees());
     }
 
     private void disableReorder() {
-        TableColumn[] columns = new TableColumn[]{new TableColumn("First Name"), new TableColumn("Last name"),
-                                                  new TableColumn("Salary"), new TableColumn("Age")};
+        ObservableList columns = table.getColumns();
+
         table.getColumns().addListener(new ListChangeListener() {
             private boolean suspended;
 
