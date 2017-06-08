2f5e2079c387eafd0a3811399c54766811c92391
SanCampos <thedrunkengungan@gmail.com>
2017-06-08 13:18:28 +0800

    feat:  addd login with enter key

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
 package main.java;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 /**
@@ -20,7 +22,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         loginStage =  primaryStage;
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/LIst.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Login.fxml"));
         Scene scene = new Scene(root, 600,  600);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Login");
