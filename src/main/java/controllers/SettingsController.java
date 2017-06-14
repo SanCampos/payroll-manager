d6c0e07811ee1407bf14fd246f5ae4fd07350cd2
SanCampos <thedrunkengungan@gmail.com>
2017-06-14 11:35:54 +0800

    feat: add file selection dialog (non-functional)

--- src/main/java/controllers/SettingsController.java
+++ src/main/java/controllers/SettingsController.java
@@ -1,8 +1,12 @@
 package main.java.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 import javafx.scene.shape.Circle;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 import static main.java.controllers.ControllerUtils.getAvatarCircle;
 
@@ -20,5 +24,11 @@ public class SettingsController {
 
     private void initAvatar() {
         prof_img.setClip(getAvatarCircle());
+    }
+
+    public void getPicFile() {
+        FileChooser chooser = new FileChooser();
+        chooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Image files (*.jpg, *.png, *.gif)" , "*.jpg", "*.png", "*.gif"));
+        File picFile = chooser.showOpenDialog(prof_img.getScene().getWindow());
     }
 }
