54f2f4a1706f717017060b78fd635dfa70e77be4
SanCampos <thedrunkengungan@gmail.com>
2017-07-24 13:40:17 +0800

    feat: tweak some things in display

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,12 @@
 package main.java;
 
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import main.java.db.Database;
-import main.java.models.Child;
-import main.java.utils.DialogUtils;
 
-import java.io.FileNotFoundException;
-import java.nio.file.FileAlreadyExistsException;
 import java.sql.SQLException;
-import java.time.LocalDate;
-import java.time.Period;
-import java.time.ZoneId;
 
 /**
  * Created by thedr on 6/6/2017.
@@ -36,8 +25,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         loginStage =  primaryStage;
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
-        Scene scene = new Scene(root, 575,  675);
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/childDisplay.fxml"));
+        Scene scene = new Scene(root, 1000,  800);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Login");
         primaryStage.show();
