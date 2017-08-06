79ea62e6a36e7a967ff420678423043a1b9c6708
SanCampos <thedrunkengungan@gmail.com>
2017-08-06 13:21:46 +0800

    feat: WOOOOOO ITS FEATURE FUCKING COMPLETE PRAISE GOD AND ST ISIDORE (now we need to close all the connections)

--- src/main/java/models/Child.java
+++ src/main/java/models/Child.java
@@ -264,24 +264,15 @@ public class Child {
         this.image = image;
     }
 
+    
     public String getCompleteName() {
-        String firstName = getfName();
-        String lastName = getlName();
-        String nickname = getNickname();
-
-        String placeholder = "PLACEHOLDER"; //Place holder for adding nickname if exists
-        String complete = firstName + placeholder + lastName;
-
-        //place nickname between first and last name if exists
-        if (nickname.length() != 0){
-            String nicknameString = " \"" + nickname + "\" ";
-            complete =  complete.replace(placeholder, nicknameString);
-
-        //if nickname does not exist
-        } else {
-            complete = complete.replace(placeholder, " ");
-        }
-        return complete;
+        return getCompleteName(getfName(), getlName(), getNickname());
+    }
+    
+    public static String getCompleteName(String firstName, String lastName, String nickname) {
+        StringBuilder completeName = nickname.length() == 0 ? new StringBuilder(firstName + " " + lastName) :
+                                                              new StringBuilder(firstName + " \"" + nickname + "\" " + lastName);
+        return completeName.toString();
     }
     
     public String getPlace_of_birth() {
